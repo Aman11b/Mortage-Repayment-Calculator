2595870ce18cc70bdaac9200e549287633320e6b
Aman11b <amansingh1100b@gmail.com>
2026-07-13 19:25:25 +0530

    initial  commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="min-h-screen bg-slate-200">
+      <h1 className="p-10 text-3xl font-bold">Mortage Repayment Calculator</h1>
+    </main>
+  );
+}
+
+export default App;
